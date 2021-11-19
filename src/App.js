28da6181b951d32bd2c9865350e9a644a62a82a6
Andreu Ramos <andreu.ramos.amengual@gmail.com>
2021-11-19 09:37:53 +0100

    install react bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from "./Header";
+import Gallery from "./Gallery";
 
 function App() {
   return (
     <div className="App">
         <Header/>
+        <Gallery/>
     </div>
   );
 }
